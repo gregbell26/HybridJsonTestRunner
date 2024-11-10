a97df10c01adfac5f99856be874d2a4701f7711b
Gregory Bell <trihard.studios@outlook.com>
2024-11-10 09:41:24 -0700

    Remove extra keys from PL results

--- autograder_utils/ResultFinalizers.py
+++ autograder_utils/ResultFinalizers.py
@@ -6,6 +6,11 @@ def gradescopeResultFinalizer(json_data):
 
 def prairieLearnResultFinalizer(json_data):
     json_data["gradable"] = True
+    # this is super hacky, but will work for now
+    json_data.pop("leaderboard", None)
+    json_data.pop("visibility", None)
+    json_data.pop("stdout_visibility", None)
+    json_data.pop("execution_time", None)
 
     total_score = 0
     for test in json_data["tests"]:
